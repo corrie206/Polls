ba992269ec695fac2a91f0f74622c1ac13373ead
corrie206 <corrie206@gmail.com>
2013-03-04 17:52:38 +0200

    some changes

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,9 @@
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import Context, loader
 from django.shortcuts import render, get_object_or_404
+from django.core.urlresolvers import reverse
 
-from polls.models import Poll
+from polls.models import Choice, Poll
 
 
 def index(request):
@@ -17,7 +18,20 @@ def detail(request, poll_id):
 
 
 def results(request, poll_id):
-    return HttpResponse("You're looking at the results of poll %s." % poll_id)
+        poll = get_object_or_404(Poll, pk=poll_id)
+        return render(request, 'polls/results.html', {'poll': poll})
+
 
 def vote(request, poll_id):
-    return HttpResponse("You're voting on poll %s." % poll_id)
+    p = get_object_or_404(Poll, pk=poll_id)
+    try:
+        selected_choice = p.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'poll': p,
+            'error_message': "You did not select a choice",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
